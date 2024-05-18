b75b72f06747a81e2fa775cd018f75e01b893674
JotaCaps <149966521+JotaCaps@users.noreply.github.com>
2024-05-18 14:23:11 -0300

    Primeiro Exercicio

--- projetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ projetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+
 Console.WriteLine("Hello, World!");
 
 int idade = 32;
@@ -6,3 +9,5 @@ double saldo = 10.4352;
 string nome = "Maria";
 
 Console.WriteLine($"{nome} tem {idade} anos e tem {saldo:F2} na conta!");
+
+Console.WriteLine(nome + " tem " + idade + " e tem " + saldo.ToString(CultureInfo.InvariantCulture) + " reais! ");
