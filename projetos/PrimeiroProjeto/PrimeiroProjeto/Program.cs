381f3050aef591478105d328ccc889684c1e416e
JotaCaps <149966521+JotaCaps@users.noreply.github.com>
2024-05-18 11:02:32 -0300

    Relembrando Lógica

--- projetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ projetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int idade = 32;
+double saldo = 10.4352;
+string nome = "Maria";
+
+Console.WriteLine($"{nome} tem {idade} anos e tem {saldo:F2} na conta!");
--- projetos/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net7.0/PrimeiroProjeto.AssemblyInfo.cs
+++ projetos/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net7.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974a843931b91f22ec82d2ec6218d6a2bd7cc0f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f06c6cddccf9a307370a532533624244ec5ab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
